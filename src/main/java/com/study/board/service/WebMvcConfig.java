a73d742f5ddd5a911882055dfaa83035cefc431e
tmdgh07 <s23028@gsm.hs.kr>
2024-07-31 10:36:30 +0900

    spring

---
+++ src/main/java/com/study/board/service/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.study.board.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/static/");
+    }
+}
